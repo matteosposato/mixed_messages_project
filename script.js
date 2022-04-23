6192918c74f8a934b280a2d505a4cd129c57f26d
matteosposato <sposato595@gmail.com>
2022-04-23 17:49:59 +0200

    Add comments

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
+//Arrays of horoscope's signs and option of fortune
 const horo_signs = ['ram', 'bull', 'twins', 'crab', 'lion', 'virgin', 'balance', 'scorpion', 'archer', 'goat', 'water bearer', 'fish'];
 const fortune_opt = ['very lucky', 'lucky','a bit lucky', 'neither lucky nor unlucky', 'a bit unlucky', 'unlucky', 'very unlucky'];
 
+//This function returns random sign
 const getRandomSign = () => {
     const randNumSign = Math.floor(Math.random() * 12);
     const randSign = horo_signs[randNumSign];
     return randSign;
-}
+};
 
+//This function returns random option of fortune
 const getRandomLucky = () => {
     const randNumLucky = Math.floor(Math.random() * 7);
     const randLucky = fortune_opt[randNumLucky];
     return randLucky;
-}
-//console.log(`randNumSign: ${randNumSign}\nrandSign : ${horo_signs[randNumSign]}`);
-//console.log(`randNumLucky: ${randNumLucky}\nYou are ${fortune_opt[randNumLucky]}.`);
+};
 
+//This function generate horoscope's message
 const horo_msg = {
+    name : '',
+    surname : '',
     sign : getRandomSign(),
-    lucky : getRandomLucky(),
+    work : getRandomLucky(),
+    healthcare : getRandomLucky(),
+    love : getRandomLucky(),
     get fullName () {
         return  this.name + " " + this.surname;
     },
@@ -26,10 +32,10 @@ const horo_msg = {
     }
 };
 
+//These two lines of code set value to name and surname of 'horo_msg' objects
+//--Values can be edited--
 horo_msg.name = 'Matteo';
 horo_msg.surname = 'Sposato';
-horo_msg.work = getRandomLucky();
-horo_msg.healthcare = getRandomLucky();
-horo_msg.love = getRandomLucky();
 
+//Output of the horoscope generated message
 console.log(horo_msg.message);
